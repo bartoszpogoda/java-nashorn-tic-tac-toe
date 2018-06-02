d99ab6b802212bec27823ee811c777f32c9c4788
bartoszpogoda <bartek.pogoda1@gmail.com>
2018-06-02 23:21:28 +0200

    executing moves, checking draw condition

---
+++ src/main/java/game/drawcondition/DrawConditionChecker.java
@@ -0,0 +1,5 @@
+package game.drawcondition;
+
+public class DrawConditionChecker {
+    
+}
---
+++ src/test/java/game/drawcondition/DrawConditionCheckerTest.java
@@ -0,0 +1,12 @@
+package game.drawcondition;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DrawConditionCheckerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
